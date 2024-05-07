e79fc83b54d0185dd3ff954d4b5a11db62eee66d
EC2 Default User <ec2-user@ip-172-31-77-29.ec2.internal>
2024-05-07 02:43:33 +0000

    some updates with the stack

--- alb_using_cdk/alb_using_cdk_stack.py
+++ alb_using_cdk/alb_using_cdk_stack.py
@@ -5,7 +5,8 @@ from aws_cdk import (
     aws_ec2 as ec2,
     aws_iam as iam,
     aws_rds as rds,
-    aws_elasticloadblancingv2 as elbv2
+    aws_elasticloadblancingv2 as elbv2,
+    CfOutput as output
     # aws_sqs as sqs,
 )
 from constructs import Construct
@@ -17,12 +18,25 @@ class AlbUsingCdkStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         # instantiating the vpc
-        my_network_vpc = ec2.Vpc(self,"my_network_vpc",
+        my_network_vpc = ec2.Vpc(self,"EngineeringVPC",
             ip_addresses=ec2.IpAddresses.cidr("10.0.0.0/18"),
                             subnet_configuration=[ec2.SubnetConfiguration(name="PublicSubnet01",subnet_type=ec2.SubnetType.PUBLIC),
                             ec2.SubnetConfiguration(name="PublicSubnet02",subnet_type=ec2.SubnetType.PUBLIC)])
         
+        internet_gateway = ec2.CfnInternetGateway(self,"internet_gateway")
+        
+        vpc_gateway_attachment = ec2.CfnVPCGatewayAttachment(self,"vpc_gateway_attachment", internet_gateway_id = "internet_gateway")
+        
         my_security_group = ec2.SecurityGroup(self,"EngineeringVpc",vpc=my_network_vpc)
+        
+        public_route_table = ec2.CfnRouteTable(self,"public_route_table",vpc_id="EngineeringVPC")
+        
+        public_route = ec2.CfnRoute(self,"public_route",route_table_id="public_route_table",gateway_id="internet_gateway")
+        
+        public_subnet_rt_assoc_1 = ec2.CfnSubnetRouteTableAssociation(self,"PublicSubnet1RTassoc",route_table_id="public_route_table",subnet_id =ec2.SubnetSelection(name="PublicSubnet01"))
+        
+        public_subnet_rt_assoc_2 = ec2.CfnSubnetRouteTableAssociation(self,"PublicSubnet2RTassoc",route_table_id="public_route_table",subnet_id =ec2.SubnetSelection(name="PublicSubnet02"))
+        
         
         # instantiating the IAM policies
         InstanceRole = iam.Role(self,"InstanceSSM",assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
@@ -45,6 +59,23 @@ class AlbUsingCdkStack(Stack):
                                             role=InstanceRole,
                                             vpc_subnets=ec2.SubnetSelection(name="PublicSubnet02"))
                                             
+        security_group_1 = ec2.SecurityGroup(self,"WebserversSG",vpc_id = "EngineeringVPC")
+        
+        application_load_balancerv2 = elbv2.ApplicationLoadBalancer(self,"EngineeringLB",vpc=my_network_vpc,security_group=security_group_1,internet_facing=True)
+        
+        target_group_1 = elbv2.CfnTargetGroup(self,"EngineeringWebservers",
+            HealthCheckEnabled=True,
+            HealthCheckPort=80,
+            HealthCheckProtocol="HTTP",
+            vpc_id="EngineeringVPC",
+            port=80,
+            protocol="HTTP",
+            targets=[elbv2.CfnTargetGroup.TargetDescriptionProperty(id="web_instance_1")]
+            )
+        
+        listener_1 = application_load_balancev2.add_listener("Listener",port=80)
+        
+        
         
         
         dirname = os.path.dirname(__file__)
@@ -63,11 +94,10 @@ class AlbUsingCdkStack(Stack):
             )
         webinitscriptasset.grant_read(web_instance_1.role)
         
-        # allow inbound HTTP traffic in security group
-        web_instance_1.connections.allow_from_any_ipv4(ec2.Port.tcp(80))
         
         
         # upload_bucket_name = CfnParameter(self, "uploadBucketName", type="String",
         #     description="The name of the Amazon S3 bucket where uploaded files will be stored.")
+        
 
     
